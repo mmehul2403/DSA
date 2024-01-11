1751d3524231484c7c00d2534cfc028a20d0524d
mmehul2403 <mmehul2403@gmail.com>
2024-01-11 09:11:02 -0500

    Basic operations of array with linear iteration O(n)

---
+++ DSA-Learning/src/main/java/com/mmehul/dsa/DSALearning/arrayproblems/ArrayBasics1.java
@@ -0,0 +1,79 @@
+package com.mmehul.dsa.DSALearning.arrayproblems;
+
+public class ArrayBasics1 {
+
+    private int[] arr;
+    private int nElems;
+
+    public ArrayBasics1(int capacity){
+        arr = new int[capacity];
+        nElems = 0;
+    }
+
+    //Insert operation in array
+    public void insert(int val){
+        arr[nElems] = val;
+        nElems++;
+    }
+
+    //display array
+    public void display(){
+        for(int j =0; j<nElems;j++)
+            System.out.print(arr[j] + " ");
+        System.out.println();
+    }
+
+    //search element in array
+    public boolean search(int searchVal){
+        int i=0;
+        for(;i<nElems;i++)
+            if(arr[i] == searchVal)
+                break;
+
+        return i != nElems;
+    }
+
+    //Delete element from array
+    public boolean delete(int val){
+        int i=0;
+        for(;i<nElems;i++)
+            if(arr[i] == val)
+                break;
+
+        if(i == nElems)
+            return false;
+
+        for(int k=i;k<nElems-1;k++)
+            arr[k] = arr[k+1];
+        nElems--;
+        return true;
+    }
+
+    public static void main(String[] args) {
+        ArrayBasics1 arr = new ArrayBasics1(10);
+        arr.insert(10);
+        arr.insert(4);
+        arr.insert(9);
+        arr.insert(16);
+        arr.insert(58);
+        arr.insert(47);
+        arr.insert(7);
+
+        //display array
+        arr.display();
+
+        //search operation in array
+        int searchVal = 44;
+        if(arr.search(searchVal))
+            System.out.println(searchVal + " present in the array");
+        else
+            System.out.println(searchVal +" not present in the array");
+
+        //delete operation in array
+        arr.delete(58);
+
+        //display array
+        arr.display();
+    }
+
+}
