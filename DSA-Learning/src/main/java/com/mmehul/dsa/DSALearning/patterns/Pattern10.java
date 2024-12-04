4c661f49ebcfa64b37b115afbe37e1ff8768d7ec
mmehul2403 <mmehul2403@gmail.com>
2024-12-03 20:20:54 -0500

    added pattern 9 and 10

---
+++ DSA-Learning/src/main/java/com/mmehul/dsa/DSALearning/patterns/Pattern10.java
@@ -0,0 +1,23 @@
+package com.mmehul.dsa.DSALearning.patterns;
+/*
+ A B C D E
+ A B C D
+ A B C
+ A B
+ A
+ * */
+public class Pattern10 {
+	public void printPattern(int n) {
+		for(int i=n; i>=1;i--) {
+			for(char c='A';c<'A'+i;c++) {
+				System.out.print(" "+c);
+			}
+			System.out.println();
+		}
+	}
+
+	public static void main(String[] args) {
+		Pattern10 pattern = new Pattern10();
+		pattern.printPattern(5);
+	}
+}
