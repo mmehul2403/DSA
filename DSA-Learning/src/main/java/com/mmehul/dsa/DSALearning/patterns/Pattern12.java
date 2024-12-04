3371e2efb9a47a353ce7d87298a085e5d8e0a5f1
mmehul2403 <mmehul2403@gmail.com>
2024-12-04 13:12:04 -0500

    pattern12 updated

---
+++ DSA-Learning/src/main/java/com/mmehul/dsa/DSALearning/patterns/Pattern12.java
@@ -0,0 +1,30 @@
+package com.mmehul.dsa.DSALearning.patterns;
+
+/*
+ * 
+ E
+ D E
+ C D E
+ B C D E
+ A B C D E
+
+ * 
+ * */
+public class Pattern12 {
+
+	public void printPattern(int n) {
+		for (int i = 1; i <= n; i++) {
+			char currChar = (char) ('A' + (n - i));
+			for (int j = 1; j <= i; j++) {
+				System.out.print(" " + currChar);
+				currChar++;
+			}
+			System.out.println();
+		}
+	}
+
+	public static void main(String[] args) {
+		Pattern12 pattern = new Pattern12();
+		pattern.printPattern(5);
+	}
+}
